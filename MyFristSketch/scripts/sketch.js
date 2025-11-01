f023c2d02504288cdecf033693529379b13a61b6
emoystner <emoystner@inside.artcenter.edu>
2025-11-01 13:25:41 -0400

    p5.js

---
+++ MyFristSketch/scripts/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(255, 0, 0);
+}
+
+// HELPER STUFF:
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
